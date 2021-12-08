338650f18f2eb84cc1861d8fb8a76c9f25f1c098
namanyt <namanyt1111@gmail.com>
2021-12-08 21:35:59 +0530

    reformatting

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@ import packageJSON from '../package.json'
 
 export const client = new Bot();
 client.logger.info('#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#');
-client.logger.info('                                                                          ');
-client.logger.info(`              • Server Management Bot ${packageJSON.version} is now Online! •       `);
 client.logger.info('');
-client.logger.info('          • Join our Discord Server for any Issues/Custom Bots •          ');
-client.logger.info(`                     ${colors.FgGreen + colors.Underscore + 'https://discord.gg/EgeZxGg6ev' + colors.Reset}                        `);
+client.logger.info(`              • Server Management Bot ${packageJSON.version} is now Online! •`);
+client.logger.info('');
+client.logger.info('          • Join our Discord Server for any Issues/Custom Bots •');
+client.logger.info(`                     ${colors.FgGreen + colors.Underscore + 'https://discord.gg/EgeZxGg6ev' + colors.Reset}`);
 client.logger.info('');
 client.logger.info('#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#');
 client.start();
